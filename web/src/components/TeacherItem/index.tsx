5083899df52528cbdf872635e4094fa8c1e88a8c
Clara <43656831+claradbessa@users.noreply.github.com>
2020-08-07 10:16:16 -0300

    Front-end da aplicação

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,43 @@
+
+import React from "react";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars0.githubusercontent.com/u/43656831?s=400&u=a1b96272163a2594709538a5b4a97d610a5850d9&v=4"
+          alt="Clara Bessa"
+        />
+        <div>
+          <strong>Clara Bessa</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
